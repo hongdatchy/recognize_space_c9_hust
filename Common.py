bcbfb2182a989e9983b3c2ccdb42384b6fcc78d4
hongdatchy <hongdatchy@gmail.com>
2021-07-05 16:02:29 +0700

    fisrt commit - lost weight file yolov4

---
+++ Common.py
@@ -0,0 +1,8 @@
+import math
+
+my_scale = 0.7
+arr = [[0, False], [math.floor(my_scale * 200), False], [math.floor(my_scale * 450), False],
+       [math.floor(my_scale * 750), False], [math.floor(my_scale * 1050), False],
+       [math.floor(my_scale * 1500), False], [math.floor(my_scale * 1920), False]]
+
+
